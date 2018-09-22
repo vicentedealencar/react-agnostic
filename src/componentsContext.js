9e2b9f444a627c5e1a9534683a21ed5206be63bc
Vicente de Alencar <vicentedealencar@gmail.com>
2018-09-22 03:15:37 -0300

    feat: remove react-blessed example

--- src/componentsContext.js
+++ src/componentsContext.js
@@ -2,7 +2,7 @@ import React from 'react'
 import defaults from './defaults'
 
 if (!React.createContext) {
-  throw new Error('React.createContext not defined. Try using react > 16')
+  throw new Error('React.createContext not defined. Try using react > 16.3.0')
 }
 
 export const {Provider, Consumer} = React.createContext({
